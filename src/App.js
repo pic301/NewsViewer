f272e8d585598edbc21042cab40ba526208d90ef
pic301 <ppic301@gmail.com>
2019-11-28 17:25:45 +0900

    add NewsItem & NewsList

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React,{useState} from 'react';
-import axios from 'axios'
+import React from 'react';
+import NewsList from './components/NewsList';
 
-function App() {
-const [data,setData] = useState(null)
 
-  return (
-    <div>
-      <div>
-        <button onClick={async() => {
-        try{
-          const res =axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=1dcc26f473c446e491f763a15e2e9db3'
-   ).then(res=> {
-            setData(res.data)
-          })
-        } catch(e){
-          console.log(e)
-        }
+const App = () => {
+  return <NewsList/>
+};
 
-        }}>불러오기</button>
-      </div>
-    {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
-    </div>
-  );
-}
+
 
 export default App;
+
+
